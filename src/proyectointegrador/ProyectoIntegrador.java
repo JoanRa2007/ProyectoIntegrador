6b745da77853992d5f102a911c4a4e94295dc915
joanra <joanra@ra07>
2024-10-22 22:16:13 -0500

    cambio

--- src/proyectointegrador/ProyectoIntegrador.java
+++ src/proyectointegrador/ProyectoIntegrador.java
@@ -10,11 +10,9 @@ package proyectointegrador;
  */
 public class ProyectoIntegrador {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+    
     }
     
 }
